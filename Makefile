eff5035830ae9769b52586a3044f05a5be5ab7c8
Kevin Kunzmann <kevinkunzmann@gmx.net>
2021-02-07 17:19:53 +0000

    ...

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ build_site:
 	Rscript -e 'source("renv/activate.R"); rmarkdown::render_site(encoding = "UTF-8")'
 	
 clean:
-	rm -rf *_cache *_files
+	rm -rf *_cache *_files figures
---
+++ util.R
@@ -0,0 +1,17 @@
+save_plot <- function(plt, name, width = 7.5, height = width*3/4, dpi = 300, ...) {
+	if (!dir.exists("figures"))
+		dir.create("figures")
+	plt <- plt + ggplot2::labs(...) # allow removal of caption etc.
+	ggplot2::ggsave(sprintf("%s/%s.jpg", "figures", name), plt, 
+					width = width, height = height, dpi = dpi)
+}
+
+theme_set(
+	theme_bw() + 
+	theme(
+		legend.position = "top",
+		panel.grid.minor = element_blank(),
+		legend.title = element_blank(),
+		plot.caption = element_text(size = 11, hjust = .5, vjust = 0)
+	)
+)
